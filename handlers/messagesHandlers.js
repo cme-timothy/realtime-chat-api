25eba037acbd1d61477b30f22227b27539e9875d
Timothy Magee <oaklessons@gmail.com>
2022-06-26 23:52:21 +0200

    partialy finished validating message

--- handlers/messagesHandlers.js
+++ handlers/messagesHandlers.js
@@ -2,16 +2,25 @@ const modelMessages = require("../models/messages.model");
 
 module.exports = (io, socket) => {
   socket.on("add_message", (data) => {
-    async function fetch() {
-      const parsedData = JSON.parse(data);
-      await modelMessages.addMessageRoom(
-        parsedData.message,
-        parsedData.room,
-        parsedData.username,
-        parsedData.timestamp
-      );
-      socket.to(parsedData.room).emit("new_message", data);
+    const parsedData = JSON.parse(data);
+    const message = typeof parsedData.message;
+    const objectLength = Object.keys(parsedData).length;
+
+    if (
+      message === "string" &&
+      objectLength === 4 &&
+      parsedData.message.length > 0
+    ) {
+      async function fetch() {
+        await modelMessages.addMessageRoom(
+          parsedData.message,
+          parsedData.room,
+          parsedData.username,
+          parsedData.timestamp
+        );
+        socket.to(parsedData.room).emit("new_message", data);
+      }
+      fetch();
     }
-    fetch();
   });
 };
