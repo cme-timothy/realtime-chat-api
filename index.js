ec5eacb82c6a079a4c340fd013b3838f2fffb767
Timothy Magee <oaklessons@gmail.com>
2022-06-26 19:44:48 +0200

    fixed user not online when creating room

--- index.js
+++ index.js
@@ -25,10 +25,15 @@ io.on("connection", (socket) => {
 
   socket.on("create_room", (data) => {
     async function fetch() {
-      await modelRooms.addRoom(data);
-      console.log(`Socket with id: ${socket.id} has joined room: ${data}`);
-      socket.join(data);
-      socket.broadcast.emit("new_room", data);
+      const parsedData = JSON.parse(data);
+      await modelRooms.addRoom(parsedData.room);
+      await modelInRoom.addUserRoom(parsedData.room, parsedData.username);
+      console.log(
+        `Socket with id: ${socket.id} has joined room: ${parsedData.room}`
+      );
+      socket.join(parsedData.room);
+      socket.broadcast.emit("new_room", parsedData.room);
+      socket.broadcast.emit("new_user_online", parsedData.username);
     }
     fetch();
   });
