7879866e3a11eae0c4af94b0cbd567592d25a321
Timothy Magee <oaklessons@gmail.com>
2022-06-28 14:58:00 +0200

    restricted creating username if it is taken or empty

--- handlers/usersHandlers.js
+++ handlers/usersHandlers.js
@@ -1,8 +1,29 @@
 const modelUsers = require("../models/users.model");
 
 module.exports = (io, socket) => {
-  socket.on("create_user", async (data) => {
-    await modelUsers.addUser(data, socket.id);
-    console.log(`Socket with id: ${socket.id} has created username: ${data}`);
+  socket.on("create_user", async (data, callback) => {
+    const name = typeof data;
+
+    if (name === "string" && data !== "") {
+      const nameTaken = await modelUsers.getUser("empty", data);
+      if (nameTaken === undefined) {
+        await modelUsers.addUser(data, socket.id);
+        console.log(
+          `Socket with id: ${socket.id} has created username: ${data}`
+        );
+
+        callback({
+          status: "ok",
+        });
+      } else {
+        callback({
+          status: "Name taken",
+        });
+      }
+    } else {
+      callback({
+        status: "Name empty",
+      });
+    }
   });
 };
