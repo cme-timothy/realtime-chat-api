2c0aaec3266d7e54e08087a713239843de61a745
Timothy Magee <oaklessons@gmail.com>
2022-06-28 16:27:02 +0200

    direct messages now possible

--- handlers/messagesHandlers.js
+++ handlers/messagesHandlers.js
@@ -1,15 +1,16 @@
 const modelMessages = require("../models/messages.model");
+const modelUsers = require("../models/users.model");
 
 module.exports = (io, socket) => {
-  socket.on("add_message", async (data) => {
+  socket.on("add_message", async (data, directMessage) => {
     const parsedData = JSON.parse(data);
     const message = typeof parsedData.message;
     const objectLength = Object.keys(parsedData).length;
-
     if (
       message === "string" &&
       objectLength === 4 &&
-      parsedData.message.length > 0
+      parsedData.message.length > 0 &&
+      directMessage === undefined
     ) {
       await modelMessages.addMessageRoom(
         parsedData.message,
@@ -18,6 +19,10 @@ module.exports = (io, socket) => {
         parsedData.timestamp
       );
       socket.to(parsedData.room).emit("new_message", data);
+      socket.to(parsedData.room).emit("user_regret", data);
+    } else {
+      const userData = await modelUsers.getUser("empty", directMessage);
+      io.to(userData.socketId).emit("direct_message", data);
       socket.to(parsedData.room).emit("user_regret", data);
     }
   });
