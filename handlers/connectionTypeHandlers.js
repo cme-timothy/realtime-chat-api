c2b3d034b5e9667ad1d05d16b0ff01317506851f
Timothy Magee <oaklessons@gmail.com>
2022-06-30 18:12:09 +0200

    fixed room not deleted bug

--- handlers/connectionTypeHandlers.js
+++ handlers/connectionTypeHandlers.js
@@ -1,17 +1,28 @@
 const modelUsers = require("../models/users.model");
 const modelInRoom = require("../models/inRoom.model");
+const modelRooms = require("../models/rooms.model");
+const modelMessages = require("../models/messages.model");
 
 module.exports = (io, socket) => {
   console.log(`Socket with id: ${socket.id} has connected`);
 
   socket.on("disconnect", async (reason) => {
-    const result = await modelInRoom.getUserRoom(socket.id);
+    const inRoomResult = await modelInRoom.getUserRoom(socket.id);
     await modelInRoom.deleteUserRoom(socket.id);
     await modelUsers.deleteUser(socket.id);
     console.log(`Socket with id: ${socket.id} disconnected. Reason: ${reason}`);
-    if (result !== undefined) {
-      socket.leave(result.room);
-      socket.broadcast.emit("user_offline", result.username);
+    if (inRoomResult !== undefined) {
+      socket.leave(inRoomResult.room);
+      socket.broadcast.emit("user_offline", inRoomResult.username);
+
+      const allUsersInRoomResult = await modelInRoom.getAllUsersRoom(
+        inRoomResult.room
+      );
+      if (allUsersInRoomResult.length === 0) {
+        await modelRooms.deleteRoom(inRoomResult.room);
+        await modelMessages.deleteMessagesRoom(inRoomResult.room);
+        io.emit("room_deleted", inRoomResult.room);
+      }
     }
   });
 };
